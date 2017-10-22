6d05d57898dd59f9bfcaf1a27e7860dce98eb7e2
白坂有己人 <shirasaka.ukito@cloudpayment.co.jp>
2017-10-23 00:37:14 +0900

    試験データのプロット

--- cp_ai_club/coursera_week3/visualize_data.py
+++ cp_ai_club/coursera_week3/visualize_data.py
@@ -1,26 +1,43 @@
 import csv
 import numpy as np
+from collections import defaultdict
 import matplotlib.pyplot as plt
 
-#　データを読み込む
-with open("ex2data1.txt",'r',newline='') as csvfile:
-    read_csv = csv.reader(csvfile)
-    y_exam1 = []
-    y_exam2 = []
-    n_exam1 = []
-    n_exam2 = []
-    for row in read_csv:
-        if(row[2] == '1'):
-            y_exam1.append(row[0])
-            y_exam2.append(row[1])
-        elif(row[2] == '0'):
-            n_exam1.append(row[0])
-            n_exam2.append(row[1])
 
-# generate data
+class csv_handler():
+    def __init__(self,file_name):
+        self.data = np.loadtxt(file_name,delimiter=",")
 
-plt.scatter(y_exam1,y_exam2,c='green', marker='o')
-plt.scatter(n_exam1,n_exam2,c='red',marker='x')
+    # 読み込んだデータをグループ化する
+    # 前提：group化されるキーは負の整数0からの連番で表現される
+    def divide_data_by_group(self,group_key_col_num):
+        #空の辞書データを作成
+        group_data_set= dict()
+        group_data_set['admitted'] = dict()
+        group_data_set['not_admitted'] = dict()
+        group_data_set['admitted']['exam1_score'] = []
+        group_data_set['admitted']['exam2_score'] = []
+        group_data_set['not_admitted']['exam1_score'] = []
+        group_data_set['not_admitted']['exam2_score'] = []
+        #空の辞書データにCSVの値を入れていく
+        for row_data in self.data:
+            if(row_data[group_key_col_num] == 1):
+                group_data_set['admitted']['exam1_score'].append(row_data[0])
+                group_data_set['admitted']['exam2_score'].append(row_data[1])
+            else:
+                group_data_set['not_admitted']['exam1_score'].append(row_data[0])
+                group_data_set['not_admitted']['exam2_score'].append(row_data[1])
+
+        return group_data_set
+
+#CSVデータを読み込む
+data = csv_handler('ex2data1.txt')
+#配列化する
+grouped_data = data.divide_data_by_group(2)
+
+# データを描画
+plt.scatter(grouped_data['admitted']['exam1_score'],grouped_data['admitted']['exam2_score'],c='green', marker='o')
+plt.scatter(grouped_data['not_admitted']['exam1_score'],grouped_data['not_admitted']['exam2_score'],c='red',marker='x')
 plt.title('Exam result',)
 plt.xlabel('exam1')
 plt.ylabel('exam2')
